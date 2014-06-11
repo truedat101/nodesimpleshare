821a5fca33a498dce4408985ca6c301f02ac26b3
David J. Kordsmeier <dkords@gmail.com>
2014-06-10 19:20:41 -0700

    Change process port to 1337 or process.env

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ var RESOURCE_PERMISSION_DENIED = "This resource is not available, permission den
 
 var js = new JS();
 js.CONFIG.DOCROOT = './static';
-js.CONFIG.HTTPWS_PORT = 80;
+js.CONFIG.HTTPWS_PORT = process.env.PORT || 1337;
 console.log(js.CONFIG);
 js.create(js.address, js.CONFIG.HTTPWS_PORT);
 js.listenHttpWS();
